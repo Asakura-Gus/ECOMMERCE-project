14ee0a69a97af03075002cd4a8808267dec6606d
Gustavo Kuthumy <79290224+Asakura-Gus@users.noreply.github.com>
2023-07-11 17:35:04 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** 
+ * @type {import ('next').NextConfig} 
+ */ 
 const nextConfig = {
-  output: 'export',
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  /* config options here */
+  output: 'export'
 }
- 
+
 module.exports = nextConfig
